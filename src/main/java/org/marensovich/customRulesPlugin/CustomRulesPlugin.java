48c350aa77a63e16956e9291377fd6c7acff6b8e
Andrey <98303133+marensovich@users.noreply.github.com>
2025-08-26 01:15:06 +0300

    init commit

---
+++ src/main/java/org/marensovich/customRulesPlugin/CustomRulesPlugin.java
@@ -0,0 +1,17 @@
+package org.marensovich.customRulesPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomRulesPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
