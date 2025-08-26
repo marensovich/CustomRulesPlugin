d1d3d8139d4df334f3d25fba383453c8be979d8c
Andrey <98303133+marensovich@users.noreply.github.com>
2025-08-26 10:46:44 +0300

    Добавлена команда /rules reload для перезагрузки плагина

---
+++ src/main/java/org/marensovich/customRulesPlugin/Commands/RulesCommand.java
@@ -0,0 +1,91 @@
+package org.marensovich.customRulesPlugin.Commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.BukkitCommand;
+import org.marensovich.customRulesPlugin.CustomRulesPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class RulesCommand extends BukkitCommand {
+
+    public RulesCommand() {
+        super("rules");
+        this.setDescription("Управление правилами плагина.");
+        this.setUsage("/rules <reload>");
+        this.setPermission("customrules.use");
+        this.setPermissionMessage(ChatColor.translateAlternateColorCodes('&',
+                Objects.requireNonNull(CustomRulesPlugin.getInstance().getConfig().getString("plugin-prefix"))) +
+                ChatColor.translateAlternateColorCodes('&',
+                        Objects.requireNonNull(CustomRulesPlugin.getInstance().getConfig().getString("no-permission"))));
+        this.setAliases(List.of("rules"));
+    }
+
+    @Override
+    public boolean execute(@NotNull CommandSender sender, @NotNull String label, @NotNull String[] args) {
+        if (!testPermission(sender)) {
+            return true;
+        }
+        if (args.length == 0) {
+            if (!sender.hasPermission("customrules.use")){
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                                        Objects.requireNonNull(CustomRulesPlugin.getInstance().getConfig().getString("plugin-prefix"))) +
+                                ChatColor.translateAlternateColorCodes('&',
+                                        Objects.requireNonNull(CustomRulesPlugin.getInstance().getConfig().getString("no-permission"))));
+                return false;
+            }
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                    Objects.requireNonNull(CustomRulesPlugin.getInstance().getConfig().getString("plugin-prefix"))) +
+                    ChatColor.translateAlternateColorCodes('&',
+                    Objects.requireNonNull(CustomRulesPlugin.getInstance().getConfig().getString("info"))));
+            return true;
+        }
+        if (args[0].equalsIgnoreCase("reload")) {
+            if (sender.hasPermission("customrules.reload")) {
+                try {
+                    CustomRulesPlugin.getInstance().reloadConfig();
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            Objects.requireNonNull(CustomRulesPlugin.getInstance().getConfig().getString("plugin-prefix"))) +
+                            ChatColor.translateAlternateColorCodes('&',
+                            Objects.requireNonNull(CustomRulesPlugin.getInstance().getConfig().getString("reload-success"))));
+                    return true;
+                } catch (Exception e) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            Objects.requireNonNull(CustomRulesPlugin.getInstance().getConfig().getString("plugin-prefix"))) +
+                            ChatColor.translateAlternateColorCodes('&',
+                            Objects.requireNonNull(CustomRulesPlugin.getInstance().getConfig().getString("reload-error"))));
+                    return false;
+                }
+            } else {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        Objects.requireNonNull(CustomRulesPlugin.getInstance().getConfig().getString("plugin-prefix"))) +
+                        ChatColor.translateAlternateColorCodes('&',
+                        Objects.requireNonNull(CustomRulesPlugin.getInstance().getConfig().getString("no-permission"))));
+                return false;
+            }
+        }
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                Objects.requireNonNull(CustomRulesPlugin.getInstance().getConfig().getString("plugin-prefix"))) +
+                ChatColor.translateAlternateColorCodes('&',
+                Objects.requireNonNull(CustomRulesPlugin.getInstance().getConfig().getString("no-permission"))));
+        return false;
+    }
+
+    @Override
+    public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) {
+        List<String> completions = new ArrayList<>();
+
+        if (args.length == 1) {
+            if (sender.hasPermission("customrules.reload")) {
+                String input = args[0].toLowerCase();
+                if ("reload".startsWith(input)) {
+                    completions.add("reload");
+                }
+            }
+        }
+        return completions;
+    }
+}
--- src/main/java/org/marensovich/customRulesPlugin/CustomRulesPlugin.java
+++ src/main/java/org/marensovich/customRulesPlugin/CustomRulesPlugin.java
@@ -1,17 +1,65 @@
 package org.marensovich.customRulesPlugin;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.marensovich.customRulesPlugin.Commands.RulesCommand;
+import org.marensovich.customRulesPlugin.Listeners.ServerJoinListener;
+
 
 public final class CustomRulesPlugin extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
+    private static CustomRulesPlugin instance;
 
+    private FileConfiguration config;
+
+    CustomRulesPlugin(){
+        instance = this;
+    }
+
+    public static CustomRulesPlugin getInstance() {
+        return instance;
+    }
+
+    public FileConfiguration getConfig() {
+        return config;
     }
 
     @Override
+    public void reloadConfig() {
+        super.reloadConfig();
+        this.config = super.getConfig();
+    }
+
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        reloadConfig();
+        getLogger().info("CustomRules plugin enabled");
+
+        registerCommands();
+        registerListeners();
+    }
+
+    private void registerListeners() {
+        getServer().getPluginManager().registerEvents(new ServerJoinListener(), this);
+    }
+
+    private void registerCommands() {
+        org.bukkit.command.Command existingCommand = getServer().getCommandMap().getCommand("rules");
+        if (existingCommand != null) {
+            existingCommand.unregister(getServer().getCommandMap());
+        }
+
+        RulesCommand rulesCommand = new RulesCommand();
+        getServer().getCommandMap().register("customrules", rulesCommand);
+
+        getLogger().info("Команда /rules успешно зарегистрирована");
+    }
+
+
+    @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("CustomRules plugin disabled");
     }
 }
